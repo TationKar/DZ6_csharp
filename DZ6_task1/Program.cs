731b214bae4666494e09431e7d6a8700f8d323f2
Timur Karimov <tationkar@gmail.com>
2023-06-08 20:56:05 +0300

    copyArr

---
+++ DZ6_task1/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Задача 45: Напишите программу,
+которая будет создавать копию заданного массива 
+с помощью поэлементного копирования. */
+Console.Clear();
+Console.Write("Введите размер массива sizeArr: ");
+int sizeArr = int.Parse(Console.ReadLine()!);
+int[] originArr = FillArray(sizeArr);
+Console.WriteLine($"[{String.Join(", ", originArr)}]");
+Console.WriteLine($"[{String.Join("| ", CopyArray(originArr))}]");
+
+int[] FillArray(int sizeArr)
+{
+    int[] resultArr = new int[sizeArr];
+    for (int i = 0; i < resultArr.Length; i++)
+    {
+        resultArr[i] = new Random().Next(10);
+    }
+    return resultArr;
+}
+
+int[] CopyArray(int[] array)
+{
+    int[] copyArr = new int[array.Length];
+    for (int i = 0; i < copyArr.Length; i++)
+    {
+        copyArr[i] = array[i];
+    }
+    return copyArr;
+}
